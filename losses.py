2233a6282cea28785cd06ea707fb2d33415beab1
AlbertoForesti <alberto.foresti.01@icloud.com>
2024-12-04 15:35:17 +0100

    Cleaned some code

--- losses.py
+++ losses.py
@@ -105,71 +105,6 @@ def get_loss_fn(noise, graph, train, sampling_eps=1e-3, lv=False, mutinfo_config
 
     return loss_fn
 
-def get_derivative_loss_fn(noise, graph, train, sampling_eps=1e-3, lv=False, mutinfo_config=None, marginal_score_fn = None, joint_score_fn = None):
-
-    def derivative_loss_fn(model, batch, cond=None, t=None, perturbed_batch=None):
-        if t is None:
-            if lv:
-                raise NotImplementedError("Yeah I gotta do this later")
-            else:
-                t = (1 - sampling_eps) * torch.rand(batch.shape[0], device=batch.device) + sampling_eps
-        
-        # print("1 - Batch example: ", batch[0])
-        # t = 1e-3*torch.ones_like(t)
-        sigma, dsigma = noise(t)
-        # raise UserWarning(f"t is {t}, sigma is {sigma}, batch shape is {batch.shape}")
-        
-        marginal_step = False
-
-        if mutinfo_config is not None:
-            marginal_step = np.random.rand() < 0.5
-            if marginal_step:
-                var_y_indices = list(mutinfo_config['y_indices'])
-                random_batch_permutation = np.random.permutation(batch.shape[0])
-                shuffled_values = batch[random_batch_permutation, var_y_indices].clone()
-                batch[np.arange(batch.shape[0]), var_y_indices] = shuffled_values
-
-        if perturbed_batch is None:
-            perturbed_batch = graph.sample_transition(batch, sigma[:, None])
-        
-        # print("2 - Batch example: ", batch[0])
-
-        log_score_fn = mutils.get_score_fn(model, train=train, sampling=False, is_marginal=marginal_step)
-        log_score = log_score_fn(perturbed_batch, sigma)
-
-        """if np.random.rand() < 1e-2:
-            if marginal_step:
-                if marginal_score_fn is None:
-                    score_analytic = None
-                else:
-                    score_analytic = marginal_score_fn(perturbed_batch, sigma)
-            else:
-                if joint_score_fn is None:
-                    score_analytic = None    
-                else:
-                    score_analytic = joint_score_fn(perturbed_batch, sigma)
-            print(f"Score example - Marginal-{marginal_step}:\n x: {perturbed_batch[0]}\n x0: {batch[0]}\n Estimated score: {log_score[0].exp()}\n True score: {score_analytic[0]}\n t: {t[0]}")
-            print(f"Mean Absolute Error: {torch.abs(log_score.exp() - score_analytic).mean()}")
-            print(f"Mean Absolute Error with marginal: {torch.abs(log_score.exp() - marginal_score_fn(perturbed_batch, sigma)).mean()}")"""
-        
-        
-        dloss = graph.derivative_score_entropy(log_score, sigma[:, None], perturbed_batch, batch)
-
-        # print("3 - Batch example: ", batch[0])
-
-        # print(f"Loss shape before dsigma stuff {loss.shape}")
-
-        dloss = (dsigma[:, None] * dloss).sum(dim=-1)
-
-        # print(f"Loss shape after dsigma stuff {loss.shape}")
-
-        # print("*****************************")
-
-        return dloss
-    
-    return derivative_loss_fn
-
-
 def get_optimizer(config, params):
     if config.optim.optimizer == 'Adam':
         optimizer = optim.Adam(params, lr=config.optim.lr, betas=(config.optim.beta1, config.optim.beta2), eps=config.optim.eps,
@@ -211,7 +146,6 @@ def optimization_manager(config):
 
 def get_step_fn(noise, graph, train, optimize_fn, accum, mutinfo_config=None, marginal_score_fn=None, joint_score_fn=None, debug=False):
     loss_fn = get_loss_fn(noise, graph, train, mutinfo_config=mutinfo_config, marginal_score_fn=marginal_score_fn, joint_score_fn=joint_score_fn)
-    derivative_loss_fn = get_derivative_loss_fn(noise, graph, train, mutinfo_config=mutinfo_config, marginal_score_fn=marginal_score_fn, joint_score_fn=joint_score_fn)
 
     accum_iter = 0
     total_loss = 0
@@ -232,18 +166,11 @@ def get_step_fn(noise, graph, train, optimize_fn, accum, mutinfo_config=None, ma
             assert not torch.isnan(loss).any(), f"Loss is NaN: {loss}"
 
             if joint_score_fn is not None and marginal_score_fn is not None and debug:
-                log_score_joint_fn = lambda x, s: joint_score_fn(x, s).log()
-                min_loss_joint = loss_fn(log_score_joint_fn, batch, cond=cond).mean()
-                log_score_marginal_fn = lambda x, s: marginal_score_fn(x, s).log()
-                min_loss_marginal = loss_fn(log_score_marginal_fn, batch, cond=cond).mean()
-                
-                # if min_loss_joint > min_loss_marginal:
-                #   print(f"Joint score loss should be lower than marginal score loss but got {min_loss_joint} > {min_loss_marginal}")
-                """dloss = derivative_loss_fn(model, batch, cond=cond).mean() / accum
-                dloss_joint = derivative_loss_fn(log_score_joint_fn, batch, cond=cond).mean() / accum
-                dloss_marginal = derivative_loss_fn(log_score_marginal_fn, batch, cond=cond).mean() / accum"""
-
                 if np.random.rand() < 1e-3:
+                    log_score_joint_fn = lambda x, s: joint_score_fn(x, s).log()
+                    min_loss_joint = loss_fn(log_score_joint_fn, batch, cond=cond).mean()
+                    log_score_marginal_fn = lambda x, s: marginal_score_fn(x, s).log()
+                    min_loss_marginal = loss_fn(log_score_marginal_fn, batch, cond=cond).mean()
                     print(f"Analytic loss joint: {min_loss_joint}, Analytic loss marginal: {min_loss_marginal}, Estimated loss: {loss}")
                     # print(f"Analytic derivative loss joint: {dloss_joint}, Analytic derivative loss marginal: {dloss_marginal}, Estimated derivative loss: {dloss}")
 
